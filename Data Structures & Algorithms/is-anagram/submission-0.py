5b0f7d40dd6d37c765b701fdeb1d47977f7eb1f3
Recursive Ray <163467727+recursiveray@users.noreply.github.com>
2026-08-17 17:01:48 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        a1 = {}
+        a2 = {}
+        for char in s:
+            if char not in a1:
+                a1[char] = 1
+            else:
+                a1[char] += 1
+        
+        for char in t:
+            if char not in a2:
+                a2[char] = 1
+            else:
+                a2[char] += 1
+                
+        if a1 == a2:
+            return True
+        else:
+            return False
+
