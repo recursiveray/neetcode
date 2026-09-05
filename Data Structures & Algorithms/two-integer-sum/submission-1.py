aa6a6321bcbbe21d1edf539d6ec30e834ca13048
Recursive Ray <163467727+recursiveray@users.noreply.github.com>
2026-09-05 09:33:04 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for idx, val in enumerate(nums):
+            complement = target - val
+
+            if complement in seen:
+                return [seen[complement], idx]
+            seen[val] = idx
